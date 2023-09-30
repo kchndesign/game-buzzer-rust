ce53436abbce2e70d6a4e2e509810b4b36f053bc
Kevin Chen <kevin@kchn.com.au>
2023-10-01 03:27:41 +1100

    Create websocket echo server using warp

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,46 +1,34 @@
-use tokio::io;
-use tokio::net::TcpListener;
-use tokio::io::AsyncReadExt;
-use tokio::io::AsyncWriteExt;
+use std::collections::HashMap;
+
+use warp::{Filter, filters::ws::WebSocket};
+use futures_util::{SinkExt, StreamExt};
 
 #[tokio::main]
-async fn main() -> io::Result<()> {
-    println!("Binding server!");
-    // bind server
-    let tcp_listener = TcpListener::bind("localhost:6142").await.unwrap();
+async fn main() {
+    // Establish in-memory state
+    let usernames = vec![""; 255];
 
-    loop { 
-        // when new clients register the connection, create a socket and adddress object
-        let (mut socket, _address) = tcp_listener.accept().await.unwrap();
-
-        // with the new socket and address, create a new async block to handle its messages.
-        // move means to capture the current closure by value.
-        tokio::spawn(async move {
-            let (mut read, mut write) = socket.split();
-            println!("split socket");
-
-            // since data that is enclosed in an await needs to be saved as a kind of closure by the Task, 
-            // we would prefer to allocate this buffer on the heap, since anything on the stack will 
-            // have to be copied for each await call.
-            let mut message_buffer = vec![0; 1025];
-            println!("Initialised msg buffer");
-
-            // enter echo loop
-            // in each loop, await receiving reads, then immediately send back 
-            loop {
-                // wait for reaad available and then read
-                let bytes_read = read.read(&mut message_buffer).await.unwrap();
-                println!("Message Received");
-                if bytes_read == 0 {
-                    return;
-                }
-                let message = String::from_utf8(message_buffer.clone()).unwrap();
-                println!("{}", message);
-
-                // write the contents of the buffer straight back into the writer.
-                write.write_all(&mut message_buffer[..bytes_read]).await.unwrap();
-                println!("Writing back");
-            }
+    // Lay out routes, middleware and actions for each route
+    let routes = warp::path("echo")
+        // ws() is a filter that will handle the handshake for 
+        // requests to this path
+        .and(warp::ws())
+        // Init a new closure with the upgrade object that 
+        // is spit out by the ws() filter
+        .map(| upgrade: warp::ws::Ws | {
+            upgrade.on_upgrade(| websocket | on_upgrade(websocket))
         });
+
+    warp::serve(routes).run(([127, 0, 0, 1], 5000)).await;
+}
+
+// handle new connections
+async fn on_upgrade(websocket: WebSocket) {
+    let (mut send, mut receive) = websocket.split();
+
+    // while there are stream elements to send, 
+    // send that element straight back to the sink
+    while let Some(message) = receive.next().await {
+        let _ = send.send(message.unwrap()).await;
     }
 }
